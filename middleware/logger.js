a1a3fb7e74a8f4a95c7a81c238ef16231c0c9ba9
massimopallera <pallera.massimo@gmail.com>
2024-12-09 11:17:43 +0100

    updated logger with other status code and colors based on status

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,28 @@
 import chalk from 'chalk';
 
-const logger =  (req,res, next) => {
-  console.log(chalk.yellow(`${req.method} request to ${req.baseUrl}. Time: [${new Date().toString()}]`));
+// const logger =  (req,res, next) => {
+//   console.log(chalk.green(`${req.method} request to ${req.baseUrl}. Time: [${new Date().toString()}]`));
+//   next()
+// }
+
+const logger = (req,res,next) => {
+
+  res.on('finish', () => {
+    //get status
+    const status = res.statusCode
+    
+    //get chalk color based on status
+    let color;
+    if(status < 300) color = chalk.green //✅ Success
+    else if (status >= 300 && status <400) color = chalk.cyan; //🔁 Redirect
+    else if (status>=400 && status<500) color = chalk.yellow  //🟥 Bad Request
+    else if (status >= 500) color = chalk.redBright //💀 Server Error
+    else color = chalk.white
+
+    //log 
+    console.log(`👉 ${chalk.blueBright( `${req.method}` )} request to ${chalk.blueBright.underline(req.baseUrl)} | Time: [${new Date().toString()}] | ${color(`Status: ${status}`)}\n` )
+  })
   next()
 }
 
-export default  logger
+export default logger
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 import express from 'express'
-const router = express.Router()
 import postsController from '../Controllers/postsController.js'
+
+const router = express.Router()
 
 router.get('/', postsController.index)
 
